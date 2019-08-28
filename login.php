b49ee4680311c01743c546c47abf3eb27ed5abd6
alexespm <manuel.espinoza@alumnos.udg.mx>
2019-08-28 14:04:19 -0500

    edicion css

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://kit.fontawesome.com/49234e04c2.js"></script>
 </head>
 <body>
 	<div id="contenedor">	
@@ -17,15 +18,19 @@
 			  <div class="form-group">
 			  	<h1>Login</h1>
 			  </div>
-			  <div class="form-group">
-			    <label>Usuario:</label>	
-			    <input type="text" name="usuario" class="form-control" aria-describedby="emailHelp" placeholder="Ingresa Usuario" required="True">
+			  <div class="form-group input-group">
+			    <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="far fa-user"></i></span>
+                </div>	
+			    <input type="text" id="usuario" name="usuario" class="form-control" placeholder="Ingresa Usuario" required="True">
 			  </div>
-			  <div class="form-group">
-			  	<label>Password:</label>
+			  <div id="usuario" class="form-group input-group">
+			    <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                </div>
 			    <input type="password" name="password" class="form-control" placeholder="Ingresa Password" required="True">
 			  </div>
-			  <div class="form-group">
+			  <div class="form-group input-group">
 			    <label class="form-check-label" for="exampleCheck1">Rol</label>
 			    <select name="rol" required="True">
 					<option value=""></option>
@@ -34,10 +39,10 @@
 					<option value="Alumno">Alumno</option>
 				</select>
 			  </div>
-			  <div>
-			  	
+			  <div id="boton">
+			  	<button type="submit" class="btn btn-primary">Submit</button>
 			  </div>
-			  <button type="submit" class="btn btn-primary">Submit</button>
+			  
 			</form>
 		</div>
 	</div>
